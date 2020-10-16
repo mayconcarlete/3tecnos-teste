c4ea39ecb743c5536a2773e781d64a62ff5a1b6f
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-16 11:17:49 -0300

    feat: ensure CreatePrefeitura return udefined if prefeitura already exists on DB

--- src/data/usecases/prefeitura/create-prefeitura.ts
+++ src/data/usecases/prefeitura/create-prefeitura.ts
@@ -3,15 +3,17 @@ import { TPrefeitura } from '@src/domain/prefeitura/model/prefeitura'
 import { ICreatePrefeitura, TPrefeituraParams } from '@src/domain/prefeitura/usecases/create-prefeitura'
 
 export class CreatePrefeitura implements ICreatePrefeitura {
- 
   private readonly loadPrefeituraByName: ILoadPrefeituraByName
- 
+
   constructor (loadPrefeituraByName: ILoadPrefeituraByName) {
     this.loadPrefeituraByName = loadPrefeituraByName
   }
 
   async create (data: TPrefeituraParams): Promise<TPrefeitura | undefined> {
     const loadPrefeituraByName = await this.loadPrefeituraByName.load(data.name)
-    return new Promise(resolve => resolve(undefined))
+    if (!loadPrefeituraByName) {
+      return undefined
+    }
+    return undefined
   }
 }
